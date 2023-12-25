9487765a06947704fdc8b78c2db5b87d300306cb
chrisjavieroliveros <chrisjavieroliveros@gmail.com>
2023-12-25 10:44:12 +0800

    svnot

--- header.php
+++ header.php
@@ -14,7 +14,7 @@
   <meta charset="<?php bloginfo('charset'); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="description" content="<?php bloginfo('description'); ?>">
-  <title><?php bloginfo('name'); ?><?php wp_title('|'); ?></title>
+  <title><?php wp_title(''); ?><?php if (!is_home()) { echo ' - '; } ?><?php bloginfo('name'); ?></title>
   <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
